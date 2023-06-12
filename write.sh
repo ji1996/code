fe1827c8b276c593b2084397f1c8e56f2a5165b2
ji1996 <59129528+ji1996@users.noreply.github.com>
2023-06-12 09:23:21 +0800

    Update write.sh

--- write.sh
+++ write.sh
@@ -2,15 +2,12 @@
 for file in ./sol/*
 do
     f=`basename ${file} .sol`
-    # contract=""
-    # echo ${f} >> done.list
     cat $file | while read row
     do
         arr=(${row})
         if [ "${arr[0]}" = "contract" ]; then
             contract=${arr[1]}
             echo ${f},${contract} >> cve.list
-            	# echo ${f} >> RE.list
         fi
     done
 done
